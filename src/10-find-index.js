e5405c425fefb32aa30443c2265f0e1582cac545
Ekaterina Pokras <eapeip@mail.ru>
2021-04-11 16:24:49 +0500

    mess

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,28 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let res;
+  let indexMax = array.length - 1;
+  let indexMin = 0;
+  let index = 0;
+  if (array[indexMax] === value) {
+    res = indexMax;
+  } else if (array[indexMin] === value) {
+    res = indexMin;
+  } else {
+    while (array[index] !== value) {
+      index = Math.floor((indexMax - indexMin) / 2);
+      if (array[index] > value) {
+        indexMax = index;
+      } else if (array[index] < value) {
+        indexMin = index;
+      } else {
+        res = index;
+      }
+    }
+  }
+  return res;
 }
 
 module.exports = findIndex;
--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,22 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let res = '';
+  let letter;
+  let count;
+  let i = 0;
+  while (i < str.length) {
+    letter = str[i];
+    count = 1;
+    i++;
+    while (str[i] === letter) {
+      count++;
+      i++;
+    }
+    res += (count > 1) ? `${count}${letter}` : letter;
+  }
+  return res;
 }
 
 module.exports = encodeLine;
